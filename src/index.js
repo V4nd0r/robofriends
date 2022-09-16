4b4ee82aeee3552ad58b8a8f7d50f9bd235c983e
Hajnalka Krekuska <krekuskahajni@freemail.hu>
2022-09-16 15:59:25 +0100

    Update index.js
    updated index file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card'; 
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>A kutya vacsorája</h1>
-    <App />
-  </React.StrictMode>
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
